a5869821c0dc455f3b54dc1902f5d33da6f5bb35
jlee512 <jlee512@aucklanduni.ac.nz>
2017-05-12 22:55:21 +1200

    added new Kata - Highest and lowest

---
+++ src/Kata.java
@@ -0,0 +1,20 @@
+/**
+ * Kata 12/05/2017
+ */
+public class Kata {
+    public static String HighAndLow(String numbers) {
+        // Code here or
+        if(numbers.length() == 1){
+            return (numbers + numbers);
+        }
+        else {
+            for (int i = 0; i < numbers; i++) {
+                
+            }
+
+
+        }
+
+        return "throw towel";
+    }
+}
