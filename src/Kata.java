24112b8f47a162281fa941ae99663f20a4950325
jlee512 <jlee512@aucklanduni.ac.nz>
2017-05-14 14:47:09 +1200

    kata progress

--- src/Kata.java
+++ src/Kata.java
@@ -4,17 +4,35 @@
 public class Kata {
     public static String HighAndLow(String numbers) {
         // Code here or
+        int max;
+        int min;
+        String[] numbersSplit = numbers.split()
         if(numbers.length() == 1){
             return (numbers + numbers);
         }
         else {
-            for (int i = 0; i < numbers; i++) {
-                
+            max = Math.max(Integer.parseInt(numbers.substring(0, 0)), Integer.parseInt(numbers.substring(1,1)));
+            min = Math.min(Integer.parseInt(numbers.substring(0, 0)), Integer.parseInt(numbers.substring(1,1)));
+            for (int i = 2; i < (numbers.length() - 1); i++) {
+                int firstNumber = Integer.parseInt(numbers.substring(i,i));
+                int secondNumber = Integer.parseInt(numbers.substring(i + 1, i + 1));
+                if (firstNumber < secondNumber){
+                    min = Math.min(min, firstNumber);
+                    max = Math.max(max, secondNumber);
+                } else {
+                    //Note, this will also catch if firstNumber == secondNumber (this will still carry out the necessary comparison
+                    min = Math.min(min, secondNumber);
+                    max = Math.max(max, firstNumber);
+                }
             }
-
-
+            return max + " " + min;
         }
+    }
 
-        return "throw towel";
+    public static void main(String[] args) {
+        Kata test = new Kata();
+        test.HighAndLow("1 2 3 4 5");
+        test.HighAndLow("1 2 -3 4 5");
+        test.HighAndLow("1 9 3 4 -5");
     }
 }
