f611181596e36d84b5bac7076f931147ab351530
jlee512 <jlee512@aucklanduni.ac.nz>
2017-05-14 15:11:57 +1200

    update after full testing and final submission

--- src/Kata.java
+++ src/Kata.java
@@ -7,22 +7,28 @@ public class Kata {
         int max;
         int min;
         String[] numbersSplit = numbers.split(" ");
-        if(numbers.length() == 1){
-            return (numbersSplit[0] + numbersSplit[0]);
+        if(numbersSplit.length == 1){
+            return (numbersSplit[0] + " " + numbersSplit[0]);
         }
         else {
             max = Math.max(Integer.parseInt(numbersSplit[0]), Integer.parseInt(numbersSplit[1]));
             min = Math.min(Integer.parseInt(numbersSplit[0]), Integer.parseInt(numbersSplit[1]));
-            for (int i = 2; i < (numbersSplit.length - 1); i++) {
-                int firstNumber = Integer.parseInt(numbersSplit[i]);
-                int secondNumber = Integer.parseInt(numbersSplit[i + 1]);
-                if (firstNumber < secondNumber){
-                    min = Math.min(min, firstNumber);
-                    max = Math.max(max, secondNumber);
-                } else {
-                    //Note, this will also catch if firstNumber == secondNumber (this will still carry out the necessary comparison
-                    min = Math.min(min, secondNumber);
-                    max = Math.max(max, firstNumber);
+            if (numbersSplit.length == 3){
+                int thirdNumber = Integer.parseInt(numbersSplit[2]);
+                max = Math.max(max, thirdNumber);
+                min = Math.min(min, thirdNumber);
+            } else {
+                for (int i = 2; i < (numbersSplit.length - 1); i++) {
+                    int firstNumber = Integer.parseInt(numbersSplit[i]);
+                    int secondNumber = Integer.parseInt(numbersSplit[i + 1]);
+                    if (firstNumber < secondNumber) {
+                        min = Math.min(min, firstNumber);
+                        max = Math.max(max, secondNumber);
+                    } else {
+                        //Note, this will also catch if firstNumber == secondNumber (this will still carry out the necessary comparison
+                        min = Math.min(min, secondNumber);
+                        max = Math.max(max, firstNumber);
+                    }
                 }
             }
             return max + " " + min;
@@ -34,5 +40,9 @@ public class Kata {
         System.out.println(test.HighAndLow("1 2 3 4 5"));
         System.out.println(test.HighAndLow("1 2 -3 4 5"));
         System.out.println(test.HighAndLow("1 9 3 4 -5"));
+        System.out.println(test.HighAndLow("1"));
+        System.out.println(test.HighAndLow("-1 -1 0"));
+        System.out.println(Math.max(0, -1));
+        System.out.println(test.HighAndLow("0 1 1"));
     }
 }
